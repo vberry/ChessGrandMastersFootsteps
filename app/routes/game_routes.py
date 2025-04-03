36ef0c887c93ab832311986d17ecdf5ef4d320d7
redah22 <124052549+redah22@users.noreply.github.com>
2025-04-03 19:43:35 +0200

    mode normal qui marche, envoie des modifs pour kevin

--- app/routes/game_routes.py
+++ app/routes/game_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, jsonify
 from app.controllers.game_controller import GameController
 from app.utils.pgn_utils import get_pgn_games, load_pgn_file
 from app.models.game_model import ChessGame
+from app.models.game_model import ChessGameNormal, ChessGameEasy
 
 game_bp = Blueprint("game", __name__)
 
@@ -23,18 +24,26 @@ def home():
 def start_game():
     game_file = request.form.get("game_file")
     user_side = request.form.get("user_side")
+    difficulty = request.form.get("difficulty", "normal")  # Par défaut: difficulté normale
 
     # Création d'un identifiant unique pour la partie
     game_id = str(len(games) + 1)
 
     game = load_pgn_file(os.path.join(pgn_dir, game_file))
-
-    # Créer la partie avec la couleur choisie
-    game_controller = GameController(game, user_side)
-    game_controller.start_game()
-
-    games[game_id] = ChessGame(game, user_side)
-    return render_template("deviner_prochain_coup.html", game_id=game_id, game_state=games[game_id].get_game_state())
+    
+    # Sélectionner la classe de jeu en fonction de la difficulté
+    
+    if difficulty == "easy":
+        games[game_id] = ChessGameEasy(game, user_side)
+        template = "deviner_prochain_coup_easy.html"
+    elif difficulty == "normal":
+        games[game_id] = ChessGameNormal(game, user_side)
+        template = "deviner_prochain_coup.html"  # Use the "hard" template for normal difficulty
+    else:  # "hard" by default
+        games[game_id] = ChessGame(game, user_side)
+        template = "deviner_prochain_coup.html"
+    
+    return render_template(template, game_id=game_id, game_state=games[game_id].get_game_state())
 
 # ✅ Route pour soumettre un coup
 @game_bp.route("/submit-move", methods=["POST"])
@@ -56,3 +65,4 @@ def submit_move():
         del result['attempts_left']
     
     return jsonify(result)
+
